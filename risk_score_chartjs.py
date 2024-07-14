758c42af621f8239660f66cd1c95eebeab169fd5
Jānis Kampars <janis.kampars@rtu.lv>
2024-07-14 13:13:21 +0300

    test charts

--- risk_score_chartjs.py
+++ risk_score_chartjs.py
@@ -43,14 +43,14 @@ def generate_annotations(start=0, end=35, color="rgba(0, 255, 0, 0.1)", text=["M
         "type": "box",
         "xMin": start,
         "xMax": end,
-        "yMin": -0.4 * maxY,
+        "yMin": -0.1 * maxY,
         "yMax": -0.04 * maxY,
         "backgroundColor": color,
         "borderWidth": 0,
         "label": {
             "display": True,
             "content": text,
-            "position": "top",
+            "position": "center",
             "color": "black",
             "textAlign": "center",
             "font": {
